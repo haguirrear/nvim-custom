ba62d195e912692a45ff0ce9ca69f20ba5be70cf
Hector Aguirre <hector.aguirre.-nd@disney.com>
2024-11-13 14:57:57 -0500

    update line numbers color in catppuccin

--- lua/custom/plugins/colors.lua
+++ lua/custom/plugins/colors.lua
@@ -2,7 +2,19 @@
 --
 
 return {
-  { "catppuccin/nvim",  name = "catppuccin", priority = 1000 },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      transparent_background = true,
+      custom_highlights = function(colors)
+        return {
+          LineNr = { fg = colors.overlay0 }, -- Color for absolute line numbers
+        }
+      end
+    }
+  },
   { 'rose-pine/neovim', name = 'rose-pine' },
 }
 -- vim: ts=2 sts=2 sw=2 et
