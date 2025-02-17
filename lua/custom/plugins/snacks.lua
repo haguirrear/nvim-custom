52e268258f2e0ec06723826bb35ff7390a9ef547
Hector Aguirre <hector.aguirre.-nd@disney.com>
2025-02-17 10:24:56 -0500

    adding snacks

---
+++ lua/custom/plugins/snacks.lua
@@ -0,0 +1,24 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    bigfile = { enabled = true },
+    dashboard = { enabled = true },
+    indent = { enabled = true },
+    input = { enabled = true },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    -- scroll = {
+    --   animate = {
+    --     duration = { step = 5, total = 80 },
+    --     easing = "linear"
+    --   }
+    -- },
+    statuscolumn = { enabled = true },
+    -- words = { enabled = true },
+  },
+}
