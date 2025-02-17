9593e456e4c39d53860b47d6892d5fb81f729860
Hector Aguirre <hector.aguirre.-nd@disney.com>
2025-02-17 11:02:04 -0500

    add nvim-surround

--- lua/lsp-setup.lua
+++ lua/lsp-setup.lua
@@ -23,7 +23,8 @@ local on_attach = function(client, bufnr)
   nmap('gr', require('custom.helpers.telescope_pickers').prettyLspReferences, '[G]oto [R]eferences')
   nmap('gI', require('custom.helpers.telescope_pickers').prettyLspImplementations, '[G]oto [I]mplementation')
   nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+  -- nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+  nmap('<leader>ds', require('custom.helpers.telescope_pickers').prettyDocumentSymbols, '[D]ocument [S]ymbols')
   nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
   -- See `:help K` for why this keymap
