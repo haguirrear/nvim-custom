36f11ca854192b1bab13486c39a28a9f3a5d94f5
Hector Aguirre <hector.aguirre.-nd@disney.com>
2025-04-12 22:02:53 -0500

    fix python

---
+++ lua/custom/plugins/nvim-lint.lua
@@ -0,0 +1,42 @@
+return {
+  "mfussenegger/nvim-lint",
+  dependencies = {
+    "williamboman/mason.nvim",
+  },
+  config = function()
+    local lint = require "lint"
+
+    lint.linters_by_ft = {
+      python = { "mypy" }
+    }
+
+    -- mypy_opts = {
+    --   -- "--strict",
+    --   -- "--check-untyped-defs",
+    -- }
+    --
+    -- for _, arg in ipairs(mypy_opts) do
+    --   table.insert(lint.linters.mypy.args, arg)
+    -- end
+
+    vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
+      callback = function()
+        -- try_lint without arguments runs the linters defined in `linters_by_ft`
+        -- for the current filetype
+        require("lint").try_lint()
+
+        -- You can call `try_lint` with a linter name or a list of names to always
+        -- run specific linters, independent of the `linters_by_ft` configuration
+        -- require("lint").try_lint("cspell")
+      end,
+    })
+
+    local function runLint()
+      require("lint").try_lint()
+    end
+
+    vim.keymap.set("n", "<leader>l", runLint, { noremap = true, desc = "Run Linter (Python)" })
+    -- vim.api.nvim_set_keymap("n", "<leader>l", "<cmd>lua require('lint').try_lint()<CR>",
+    --   { noremap = true, desc = "Run Linter (Python)" })
+  end
+}
--- lua/lsp-setup.lua
+++ lua/lsp-setup.lua
@@ -134,16 +134,21 @@ local servers = {
   pyright = {
     pyright = {
       autoImportCompletion = true,
+      -- This is managed by ruff
+      disableOrganizeImports = true,
     },
     python = {
       analysis = {
-        autoSearchPaths = true,
-        diagnosticMOde = "openFilesOnly",
-        useLibraryCodeForTypes = true,
-        typeCheckingMode = "on",
+        -- Ignore all files for analysis to exclusively use Ruff for linting
+        ignore = { '*' },
+        typeCheckingMode = "off",
+        -- autoSearchPaths = true,
+        -- diagnosticMOde = "openFilesOnly",
+        -- useLibraryCodeForTypes = true,
       },
     },
   },
+  -- For Linting and formatting
   ruff = {},
   -- rust_analyzer = {},:warnings
   -- tsserver = {
@@ -268,5 +273,22 @@ mason_lspconfig.setup_handlers {
   end
 
 }
+
+
+-- For python
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client == nil then
+      return
+    end
+    if client.name == 'ruff' then
+      -- Disable hover in favor of Pyright
+      client.server_capabilities.hoverProvider = false
+    end
+  end,
+  desc = 'LSP: Disable hover capability from Ruff',
+})
 
 -- vim: ts=2 sts=2 sw=2 et
